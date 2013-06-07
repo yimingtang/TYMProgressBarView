156c8cf292c9068065e504e1b7fdd70776ee3d60
Yiming Tang <yimingnju@gmail.com>
2013-06-07 18:40:53 +0800

    Initial Commit

---
+++ YTProgressBarViewDemo/YTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YTAppDelegate.h
+//  YTProgressBarViewDemo
+//
+//  Created by Yiming Tang on 13-6-7.
+//  Copyright (c) 2013年 Yiming Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YTProgressBarViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YTProgressBarViewDemo
+//
+//  Created by Yiming Tang on 13-6-7.
+//  Copyright (c) 2013年 Yiming Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YTAppDelegate class]));
+    }
+}
